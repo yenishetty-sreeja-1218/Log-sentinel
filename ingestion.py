285c1e9efdfccceb865f40ca41ebc6cfd910ae2f
yenishetty-sreeja-1218 <yenishettysreeja@gmail.com>
2026-03-25 22:16:17 +0530

    Add files via upload

---
+++ ingestion.py
@@ -0,0 +1,14 @@
+import dask.dataframe as dd
+
+# Load logs
+df = dd.read_csv("logs/app_logs.csv")
+
+print("\n✔ Logs Loaded Successfully")
+print(df.head())
+
+# Basic stats
+print("\n✔ Log Level Count:")
+print(df["level"].value_counts().compute())
+
+print("\n✔ Service-wise Avg Response Time:")
+print(df.groupby("service")["response_time_ms"].mean().compute())
